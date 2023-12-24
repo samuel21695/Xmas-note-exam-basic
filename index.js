7760b75ef5868202bfcb0df2ebbca7e62cf8563f
samuel21695 <samuel21695@gmail.com>
2023-12-24 18:24:07 +0900

    ✨ const config

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ class ForExam {
 
   initData() {
 
+    const config = {
+      baseArray: ['피카츄', '라이츄', '꼬부기'],
+      specialProblems: [10, 16, 17, 18, 19, 20],
+      htmlTagProblems: [22, 23, 25, 27, 29, 30],
+      startProblemIndex: 1,
+      endProblemIndex: 30
+    };
+
   }
   
 }
